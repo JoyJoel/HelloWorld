f11faf9df9bdbf01793e383b4d230ee36e4fedac
JoyJoel <zandy212@126.com>
2017-11-28 20:17:48 +0800

    三合一

--- chapter24/relationship.py
+++ chapter24/relationship.py
@@ -1,5 +1,6 @@
+"""类间关系-依赖
+"""
 from datetime import date
-#类之间的关系
 
 class Project:
     def __init__(self, name, team, start_date):
@@ -18,8 +19,7 @@ class Department:
         self.employees = []
 
     def __repr__(self):
-        return f'<Department : {self.name}>'
-
+        return f'<Department :{self.name}>'
 
 class Developer:
     def __init__(self, department, name, skills):
@@ -29,22 +29,24 @@ class Developer:
         self.department.employees.append(self)
 
     def __repr__(self):
-        return f'<Developer : {self.name}'
+        return f'<Developer :{self.name}>'
 
     def develop_project(self, project):
-        print(f'{self.name}正在参加开发项目:{project}')
+        print(f'{self.name} 正在参与开发项目: {project}')
 
 if __name__ == '__main__':
-    dpt = Department('技术部', 'Mike', '010-88889999')
-    d = Developer(dpt, 'Tom', ['Python', 'SQL', 'Flask'])
-    d2 = Developer(dpt, 'Jerry', ['Django', 'MongoDB'])
-
-    print(dpt.employees)
-
-    # print(dpt)
+    # d = Developer('Tom', ['Python', 'SQL', 'Flask'])
     # print(d)
-    # print(d.department.tel)
-
+    # d.develop_project('OA')
+    #
     # proj = Project('OA', 'DEV02', date(2017, 3, 5))
     # d.develop_project(proj)
 
+    dpt = Department('技术部', 'Mike', '010-88889999')
+    d = Developer(dpt, 'Tom', ['Python','Flask'])
+    d2 = Developer(dpt, 'Jerry', ['Django', 'MongoDB'])
+
+    print(dpt.employees)
+    # print(dpt)
+    # print(d)
+    # print(d.department.tel)
