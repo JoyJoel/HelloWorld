2dad9d5a2554f5cd9f8fab0f98d4ad34f4001545
JoyJoel <zandy212@126.com>
2017-12-06 15:13:15 +0800

    Initial commit

--- chapter34/函数定义装饰器.py
+++ chapter34/函数定义装饰器.py
@@ -5,10 +5,23 @@
 # '**kwargs'表示所有有名称的传递进来的多个参数
 # *args **kwargs "参数通配符",涵盖了函数参数的任意形式
 
+# 参数化装饰器
+def tags(tag):
+    def tag_decorator(func):
+        def wrapper(*args, **kwargs):
+            return f'<{tag}>{func(*args, **kwargs)}</{tag}>'
+        return wrapper
+    return tag_decorator
+
 # 方法一: @装饰器
 def p_decorator(func):
     def wrapper(*args, **kwargs):
         return '<p>' + func(*args, **kwargs) + '</p>'
+    return wrapper
+
+def div_decorator(func):
+    def wrapper(*args, **kwargs):
+        return '<div>{}</div>'.format(func(*args, **kwargs))
     return wrapper
 
 @p_decorator
@@ -41,7 +54,18 @@ def p_decorator(func):
 def get_text():
     return '欢迎学习优品课堂课程'
 
-@p_decorator
+@div_decorator # 后执行
+@p_decorator   # 先执行
+def get_upper_text(text):
+    return text.upper()
+
+if __name__ == '__main__':
+    print(get_upper_text('www.baidu.com'))
+
+# 例四(装饰器参数):
+@tags('body')
+@tags('div')
+@tags('p')
 def get_upper_text(text):
     return text.upper()
 
