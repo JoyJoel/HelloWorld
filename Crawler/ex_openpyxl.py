b8dbea25bbf6f877fe397ce3029429d5f9b2854f
JoyJoel <zandy212@126.com>
2017-12-13 19:26:52 +0800

    Initial commit

--- Crawler/ex_openpyxl.py
+++ Crawler/ex_openpyxl.py
@@ -1,6 +1,6 @@
 # 工作簿 Workbook
 #     工作表 Worksheet
-#         标题 Title
+#         标题    Title
 #         行      Row
 #         列      Column
 #         值      Value
@@ -13,8 +13,8 @@ wb = openpyxl.Workbook()
 ws = wb.active  # 当前在正在激活的
 ws.title = '课程'
 ws['A1'].value = 'xuepy.com'  # A列第1行
-ws['C5'] = '优品学派' # C列第5行
-ws.save(r'course.xlsx')
+ws['C5'] = '优品学派'   # C列第5行
+wb.save(r'course.xlsx')
 
 # for in 遍历数据至excel
 import openpyxl
@@ -23,5 +23,5 @@ ws = wb.active
 ws.title = '测试数据'
 for i in range(1, 11):
     for j in range(1, 6):
-        ws.cell(row=i, Column=j, value=f'数据 {i}-{j}')
+        ws.cell(row=i, column=j, value=f'数据 {i}-{j}')
 wb.save(r'data.xlsx')
