be31a84991d54f3baf3f4d26719c4f95ab5bf619
JoyJoel <zandy212@126.com>
2018-01-08 01:50:00 +0800

    Initial commit

--- chapter17/Lambda_语法.py
+++ chapter17/Lambda_语法.py
@@ -4,23 +4,19 @@ def hello():
     print('Hello')
 hello()
 
-print('---------------------------------')
 
 greeting = lambda :print('Hello')
 print(type(greeting))
 
-print('---------------------------------')
 
 def func(a, b):
     return a + b
 print(func(3, 5))
 
-print('---------------------------------')
 
 action = lambda a, b: a + b
 print(action(3,5))
 
-print('---------------------------------')
 
 action = lambda x : x ** 2
 print(action(4))
