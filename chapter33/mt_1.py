8f4b85574d260cba168f2787b1c100ba707084a1
JoyJoel <zandy212@126.com>
2018-11-19 09:12:06 +0800

    2018/11/19

--- chapter33/mt_1.py
+++ chapter33/mt_1.py
@@ -7,10 +7,12 @@ import _thread
 
 """_thread 实现多线程"""
 
+
 def worker(n):
     print(f'函数执行开始于: {time.ctime()}')
     time.sleep(n)
     print(f'函数执行结束于: {time.ctime()}')
+
 
 def main():
     print(f'【主函数执行开始于: {time.ctime()}】')
@@ -19,6 +21,7 @@ def main():
 
     time.sleep(4)
     print(f'【主函数执行结束于: {time.ctime()}】')
+
 
 if __name__ == '__main__':
     main()
